6a10b14f3dadebd4b49aaae91913c9a5055b4bff
Blandin3 <b.iradukund3@alustudent.com>
2025-03-11 10:35:15 +0200

    added middleware folder to ensure authentification

---
+++ backend/middleware/ensureAuthenticated.js
@@ -0,0 +1,9 @@
+const ensureAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.status(401).json({ error: "Unauthorized. Please log in." });
+  };
+  
+  module.exports = ensureAuthenticated;
+  
